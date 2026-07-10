0ab6bdc933546ea96d97987de08c2f2835c50d5b
Md Mominul Islam <mominulislam99255@gmail.com>
2026-07-11 00:21:18 +0600

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,20 @@
 <?php
-$host = "localhost";
-$user = "root";  // XAMPP er default user
-$pass = "";      // XAMPP er default password empty thake
-$dbname = "ecommerce_db"; // Database er naam jeta phpMyAdmin e banayecho
-$port = 3307;
-// Connection create kora
+
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
+
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
-// Connection check kora
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Session start kore dilam, jate sob page e user login track kora jay
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
