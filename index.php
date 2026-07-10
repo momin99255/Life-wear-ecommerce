d0192220f062ebb70461c76bbb3f6febf5f81ff5
Md Mominul Islam <mominulislam99255@gmail.com>
2026-07-11 01:07:39 +0600

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include 'db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="favicon.png">
-    <title>Plus Point Clone</title>
+    <title>Life Wear</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
